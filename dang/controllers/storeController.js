280509199a717ab2d46681c29b9bebc7bc623bd3
Julian Muratori <julianmuratori@gmail.com>
2018-05-17 12:12:50 -0400

    beginning of lesson 16

--- dang/controllers/storeController.js
+++ dang/controllers/storeController.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose')
 const Store = mongoose.model('Store')
 
-exports.homePage = (req, res) => {
-    console.log(req.name)
+exports.homePage = (req, res) => {    
     res.render('index');
 }
 
@@ -11,7 +10,29 @@ exports.addStore = (req, res) => {
 }
 
 exports.createStore = async (req, res) => {
-    const store = new Store(req.body)
-    await store.save()
-    console.log('It worked!')
+    const store = await (new Store(req.body)).save()
+    req.flash('success', `Successfully created ${store.name}. Care to leave a review?`)
+    res.redirect(`/store/${store.slug}`)
+}
+
+exports.getStores = async (req, res) => {
+    const stores = await Store.find()
+    console.log(stores)
+    res.render('stores', { title: 'Stores', stores })
+}
+
+exports.editStore = async (req, res) => {
+    const store = await Store.findOne({ _id: req.params.id})
+    
+    res.render('editStore', { title: `Edit ${store.name}`, store })
+}
+
+exports.updateStore = async (req, res) => {
+    const store = await Store.findOneAndUpdate({ _id: req.params.id}, req.body, {
+        new: true,
+        runValidators: true
+    }).exec()
+    
+    req.flash('success', `Successfully updated ${store.name}`)
+    res.redirect(`/stores/${store._id}/edit`)
 }
--- dang/models/Store.js
+++ dang/models/Store.js
@@ -13,8 +13,28 @@ const storeSchema = new mongoose.Schema({
         type: String,
         trim: true
     },
-    tags: [String]
+    tags: [String],
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    location: {
+        type: {
+            type: String,
+            default: 'Point'
+        },
+        coordinates: [{
+            type: Number,
+            required: 'Please supply coordinates'
+        }],
+        address: {
+            type: String,
+            required: 'You must supply an address'
+        }
+    }
 })
+
+
 
 storeSchema.pre('save', function(next) {
     if (!this.isModified('name')) {
